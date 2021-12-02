26e3d78fee9c7ce0c68044d22f255b6446077dfc
railamenezes <railamenezes02@gmail.com>
2021-12-02 18:12:38 -0300

    atividade java

---
+++ IdeaProjects/Exercicio_1_Oper/src/exercicioOper.java
@@ -0,0 +1,7 @@
+public class exercicioOper {
+    public static void main(String[] args){
+        System.out.println("Nome completo: " + "Raíla Rocha de Menezes");
+        System.out.println("Cidade Natal: " + "Pindobaçu - BA" );
+        System.out.println("Hobby Favorito: " + "Assistir Filmes, séries e ler") ;
+    }
+}
