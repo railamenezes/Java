3906aba5ae5c2dc387a34a5795eb08060682fc54
railamenezes <railamenezes02@gmail.com>
2021-12-08 21:41:17 -0300

    java

---
+++ Matriz/src/Matriz.java
@@ -0,0 +1,3 @@
+string nomes[] = {'Heloisa', 'Duda', 'Rafa'}; // Array
+
+string nomes[][] = {{'Heloisa', 'Helo', 21} {'Duda', 'du', 32} {'Rafa', 'Ra', 35}} // Matriz
---
+++ Projeto_Matriz_Explicação/src/Matriz.java
@@ -0,0 +1,20 @@
+public class Matriz {
+    public static void main (String[] args){
+
+        //String momes[] = {'Heloisa', 'Duda', 'Rafa'}; //Array
+
+        //  Forma incorreta de declarar uma matriz
+        // Char nome [][] = {{'Heloisa', 'Helo', 21} {'Duda', 'du', 32} {'Rafa', 'Ra', 35}} //Matriz
+
+        // Forma correta de declarar uma Matriz
+        // Tipo de identificador [][] = new tipo {{TamLinha}, {TamCol}}
+
+        double numeros [][] = new double [3][3];
+
+        int numeros2 [][] = new int [8][9];
+
+        int notas [][] = {{8,9,9}, {4, 7, 9}, {2,5,7}};
+    }
+}
+
+       
