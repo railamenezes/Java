9f1fc807967863486363a54e3c2e7c7ef77e943c
railamenezes <railamenezes02@gmail.com>
2021-12-14 11:12:16 -0300

    Atividade Java

---
+++ Exercicio_1_Oper/src/exercicioOper.java
@@ -0,0 +1,7 @@
+public class exercicioOper {
+    public static void main(String[] args){
+        System.out.println("Nome completo: " + "Josiane Xavier de Lima");
+        System.out.println("Cidade Natal: " + "São Paulo" );
+        System.out.println("Hobby Favorito: " + "Assistir Filmes e Animes") ;
+    }
+}
---
+++ Exercicio_2_Oper/src/exercicioOper2.java
@@ -0,0 +1,13 @@
+public class exercicioOper2 {
+    public static void main(String[]args){
+        System.out.println("Idade: " + 19);
+        System.out.println("Altura: " + 1.63);
+        String Status = "O";
+        if(Status == "O"){
+            System.out.println("Status da aula: " + "Online");
+        }if(Status == "F"){
+            System.out.println("Status da aula:" + "Offline");
+        }
+    }
+
+}
